e0b4480312a9eacc80f8156c7d0a93f203024b14
senbonzakura4133 <56403985+senbonzakura4133@users.noreply.github.com>
2020-11-03 17:18:41 +0530

    Add files via upload

---
+++ login/src/com/candid/LoginController.java
@@ -0,0 +1,32 @@
+package com.candid;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/LoginController")
+public class LoginController extends HttpServlet {
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String un=request.getParameter("username");
+		String pw=request.getParameter("password");
+		Cookie c1=new Cookie("userName",un);
+          	Cookie c2=new Cookie("userPassword",pw);
+		
+		if(un.equals("admin") && pw.equals("admin"))
+		{
+			response.sendRedirect("success.html");
+			return;
+		}
+		else
+		{
+			response.sendRedirect("error.html");
+			return;
+		}
+	}
+
+}
